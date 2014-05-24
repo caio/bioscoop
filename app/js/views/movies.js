9af8dfb2abf58fa1339cd8d6b512430217244d57
Caio Romão <caioromao@gmail.com>
2014-05-24 20:25:19 +0200

    Render all movies models inside the Movies view

--- app/js/views/movies.js
+++ app/js/views/movies.js
@@ -1,9 +1,17 @@
 MovieList.Views.Movies = Backbone.View.extend({
     template: _.template($('#tmpl-movies').html()),
 
+    renderMovie: function(movie) {
+        var movieview = new MovieList.Views.Movie({ model: movie });
+        this.$('.movies-container').append(movieview.render().$el);
+    },
+
     render: function() {
         var html = this.template();
         this.$el.html(html);
+
+        this.collection.each(this.renderMovie, this);
+
         return this;
     },
 });

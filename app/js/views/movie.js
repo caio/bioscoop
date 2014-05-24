587ec4e0e53e87be80216e0676d98bb148a17a8e
Caio Romão <caioromao@gmail.com>
2014-05-24 20:18:51 +0200

    Add fancier template for Movie model

--- app/js/views/movie.js
+++ app/js/views/movie.js
@@ -1,7 +1,11 @@
 MovieList.Views.Movie = Backbone.View.extend({
+    tagName: 'li',
+    className: 'media col-md-6 col-lg-4',
+    template: _.template($('#tmpl-movie').html()),
+
     render: function() {
-        var html = '<h1>' + this.model.get('title') + '</h1>';
-        this.$el.html(html);
+        var html = this.template(this.model.toJSON());
+        this.$el.append(html);
         return this;
     }
 });

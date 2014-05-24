e9f6aa5489ba3b18e886f50badc0a34573ac9df5
Caio Romão <caioromao@gmail.com>
2014-05-24 22:44:31 +0200

    Use data.js instead of mockdata.js

---
+++ data.js
@@ -0,0 +1,249 @@
+var movies = [
+  {
+    "directors": [
+      "John Carney"
+    ],
+    "genres": [
+      "Drama",
+      "Music",
+      "Romance"
+    ],
+    "id": 215,
+    "imdbnumber": "tt0907657",
+    "lastplayed": "2014-05-10 21:38:24",
+    "playcount": 1,
+    "plot": "The Guy is a Dublin guitarist/singer-songwriter who makes a living by fixing vacuum cleaners in his Dad's Hoover repair shop by day, and singing and playing for money on the Dublin streets by night. The Girl is a Czech who plays piano when she gets a chance, and does odd jobs by day and takes care of her mom and her daughter by night.",
+    "title": "Once",
+    "year": 2006
+  },
+  {
+    "directors": [
+      "Jerome Robbins",
+      "Robert Wise"
+    ],
+    "genres": [
+      "Musical"
+    ],
+    "id": 202,
+    "imdbnumber": "tt0055614",
+    "lastplayed": "2014-04-21 21:30:35",
+    "playcount": 1,
+    "plot": "In the slums of the upper West Side of Manhattan, New York, a gang of Polish-American teenagers called the Jets compete with a rival gang of recently immigrated Puerto Ricans, the Sharks, to \"own\" the neighborhood streets. Tensions are high between the gangs but two kids, one from each rival gang, fall in love leading to tragedy.",
+    "title": "West Side Story",
+    "year": 1961
+  },
+  {
+    "directors": [
+      "Malik Bendjelloul"
+    ],
+    "genres": [
+      "Documentary"
+    ],
+    "id": 96,
+    "imdbnumber": "tt2125608",
+    "lastplayed": "2014-02-05 20:45:54",
+    "playcount": 0,
+    "plot": "Two South Africans set out to discover what happened to their unlikely musical hero, the mysterious 1970s rock 'n' roller, Rodriguez.",
+    "title": "Searching for Sugar Man",
+    "year": 2012
+  },
+  {
+    "directors": [
+      "Bernard Rose"
+    ],
+    "genres": [
+      "Drama",
+      "Music",
+      "Romance",
+      "Foreign"
+    ],
+    "id": 92,
+    "imdbnumber": "tt0110116",
+    "lastplayed": "",
+    "playcount": 1,
+    "plot": "The life and death of the legendary Ludwig van Beethoven. Beside all the work he is known for, the composer once wrote a famous love letter to a nameless beloved and the movie tries to find out who this beloved was. Not easy as Beethoven has had many women in his life.",
+    "title": "Immortal Beloved",
+    "year": 1994
+  },
+  {
+    "directors": [
+      "Sam Taylor-Johnson"
+    ],
+    "genres": [
+      "Drama",
+      "Music",
+      "Foreign"
+    ],
+    "id": 94,
+    "imdbnumber": "tt1266029",
+    "lastplayed": "2014-02-08 00:29:05",
+    "playcount": 0,
+    "plot": "The drama tells the story of Lennon's teenage years and the start of his journey to becoming a successful musician. The story also examines the impact on his early life and personality of the two dominant females in his childhood",
+    "title": "Nowhere Boy",
+    "year": 2009
+  },
+  {
+    "directors": [
+      "Stephen Daldry"
+    ],
+    "genres": [
+      "Comedy",
+      "Drama",
+      "Music"
+    ],
+    "id": 35,
+    "imdbnumber": "tt0249462",
+    "lastplayed": "2014-01-20 20:20:25",
+    "playcount": 0,
+    "plot": "A talented young boy becomes torn between his unexpected love of dance and the disintegration of his family.",
+    "title": "Billy Elliot",
+    "year": 2000
+  },
+  {
+    "directors": [
+      "Alan Parker"
+    ],
+    "genres": [
+      "Drama",
+      "Music"
+    ],
+    "id": 36,
+    "imdbnumber": "tt0101605",
+    "lastplayed": "2014-01-21 21:33:22",
+    "playcount": 0,
+    "plot": "Jimmy Rabbitte, just a tick out of school, gets a brilliant idea: to put a soul band together in Barrytown, his slum home in north Dublin. First he needs musicians and singers: things slowly start to click when he finds three fine-voiced females virtually in his back yard, a lead singer (Deco) at a wedding, and, responding to his ad, an aging trumpet player, Joey \"The Lips\" Fagan.",
+    "title": "The Commitments",
+    "year": 1991
+  },
+  {
+    "directors": [
+      "Lonny Price"
+    ],
+    "genres": [
+      "Music"
+    ],
+    "id": 32,
+    "imdbnumber": "tt1942831",
+    "lastplayed": "2014-01-15 22:23:51",
+    "playcount": 0,
+    "plot": "Following five couples and their friend Robert (Neil Patrick Harris), the perpetual bachelor, Company explores the true meaning of being in a relationship through a series of vignettes. Winner of the 1971 Tony Award for Best Musical, Neil Patrick Harris (How I Met Your Mother, Rent) will led an all-star cast in a sold out event at the New York Philharmonic.",
+    "title": "Company",
+    "year": 2011
+  },
+  {
+    "directors": [
+      "Tim Burton"
+    ],
+    "genres": [
+      "Drama",
+      "Horror",
+      "Music"
+    ],
+    "id": 30,
+    "imdbnumber": "tt0408236",
+    "lastplayed": "2014-05-20 20:58:14",
+    "playcount": 0,
+    "plot": "The infamous story of Benjamin Barker, a.k.a Sweeney Todd, who sets up a barber shop down in London which is the basis for a sinister partnership with his fellow tenant, Mrs. Lovett. Based on the hit Broadway musical.",
+    "title": "Sweeney Todd: The Demon Barber of Fleet Street",
+    "year": 2007
+  },
+  {
+    "directors": [
+      "Joel Schumacher"
+    ],
+    "genres": [
+      "Drama",
+      "Musical"
+    ],
+    "id": 28,
+    "imdbnumber": "tt0293508",
+    "lastplayed": "2014-05-20 20:58:17",
+    "playcount": 1,
+    "plot": "Deformed since birth, a bitter man known only as the Phantom lives in the sewers underneath the Paris Opera House. He falls in love with the obscure chorus singer Christine, and privately tutors her while terrorizing the rest of the opera house and demanding Christine be given lead roles. Things get worse when Christine meets back up with her childhood acquaintance Raoul and the two fall in love",
+    "title": "The Phantom of the Opera",
+    "year": 2004
+  },
+  {
+    "directors": [
+      "Tom Hooper"
+    ],
+    "genres": [
+      "Drama",
+      "Music",
+      "Romance"
+    ],
+    "id": 26,
+    "imdbnumber": "tt1707386",
+    "lastplayed": "2014-05-20 20:58:19",
+    "playcount": 0,
+    "plot": "An adaptation of the successful stage musical based on Victor Hugo's classic novel set in 19th-century France, in which a paroled prisoner named Jean Valjean seeks redemption.",
+    "title": "Les Mis\u00e9rables",
+    "year": 2012
+  },
+  {
+    "directors": [
+      "Julie Taymor"
+    ],
+    "genres": [
+      "Adventure",
+      "Drama",
+      "Music",
+      "Romance"
+    ],
+    "id": 24,
+    "imdbnumber": "tt0445922",
+    "lastplayed": "2014-01-12 18:49:16",
+    "playcount": 1,
+    "plot": "Musical based on The Beatles songbook and set in the 60s England, America, and Vietnam. The love story of Lucy and Jude is intertwined with the anti-war movement and social protests of the 60s.",
+    "title": "Across the Universe",
+    "year": 2007
+  },
+  {
+    "directors": [
+      "Lars von Trier"
+    ],
+    "genres": [
+      "Crime",
+      "Drama",
+      "Music"
+    ],
+    "id": 25,
+    "imdbnumber": "tt0168629",
+    "lastplayed": "2014-01-13 22:02:20",
+    "playcount": 1,
+    "plot": "Selma, a Czech immigrant on the verge of blindness, struggles to make ends meet for herself and her son, who has inherited the same genetic disorder and will suffer the same fate without an expensive operation. When life gets too difficult, Selma learns to cope through her love of musicals, escaping life's troubles - even if just for a moment - by dreaming up little numbers to the rhythmic beats of her surroundings.",
+    "title": "Dancer in the Dark",
+    "year": 2000
+  },
+  {
+    "directors": [
+      "Michael John Warren"
+    ],
+    "genres": [
+      "Music",
+      "Romance"
+    ],
+    "id": 23,
+    "imdbnumber": "tt1273675",
+    "lastplayed": "2014-01-12 16:38:31",
+    "playcount": 1,
+    "plot": "Based on Puccini's opera La boh\u00e8me, Rent tells the story of a group of impoverished young artists struggling to survive and create in New York City in the 1990s. This is a live recording of Rent's final performance after more than 12 years and 5000 shows.",
+    "title": "Rent: Filmed Live on Broadway",
+    "year": 2008
+  },
+  {
+    "directors": [
+      "Baz Luhrmann"
+    ],
+    "genres": [
+      "Music"
+    ],
+    "id": 22,
+    "imdbnumber": "tt0203009",
+    "lastplayed": "2014-01-11 16:34:30",
+    "playcount": 0,
+    "plot": "A poet falls for a beautiful courtesan whom a jealous duke covets in this stylish musical, with music drawn from familiar 20th century sources.",
+    "title": "Moulin Rouge!",
+    "year": 2001
+  }
+];

2256efb6b6fae51b7f65522c4d0d0b70f8231b6e
Caio Romão <caioromao@gmail.com>
2014-05-24 19:43:11 +0200

    Initialize mock data via start()

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,14 @@ window.MovieList = {
     Models: {},
     Collections: {},
     Views: {},
+
+    start: function(setup) {
+        console.log("Starting MovieList");
+
+        var movies = new MovieList.Collections.Movies(setup.movies);
+
+        movies.each(function (movie) {
+            console.log(">>> " + movie.get("title"));
+        });
+    },
 };

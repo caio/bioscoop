fdfe6cf9d25e69583d83e2ce64869ae77e2c6478
Caio Romão <caioromao@gmail.com>
2014-05-25 11:29:45 +0200

    Remove thumbnail click handler. Unnecessary

--- app/js/views/movie.js
+++ app/js/views/movie.js
@@ -3,15 +3,6 @@ MovieList.Views.Movie = Backbone.View.extend({
     className: 'media col-md-3 col-lg-3 col-sm-3',
     template: _.template($('#tmpl-movie').html()),
 
-    events: {
-        'click .view-movie-details': 'onClickDetails',
-    },
-
-    onClickDetails: function(ev) {
-        ev.preventDefault();
-        console.log("Click registered on " + this.model.get('id'));
-    },
-
     render: function() {
         var html = this.template(this.model.toJSON());
         this.$el.append(html);

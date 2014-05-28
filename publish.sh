395fe14d5ec96b3374afeb5a2514fa1d824caa15
Caio Romão <caioromao@gmail.com>
2014-05-28 15:47:53 -0300

    Revert "Lazy load images to avoid blowing up my limited bandwidth"
    Causing problems when browsing the galery (the lazyload() needs to
    be called in each render(), but just putting it on  the end of the
    listMovies route is not working, nor just putting it on the render()
    function of the Movies view.
    
    This reverts commit 0b7cb542bd69ff6f6ad2ac0ebf9ac6ddc19cc53e.

--- publish.sh
+++ publish.sh
@@ -7,7 +7,6 @@ rsync -avRt \
     index.html \
     vendor/bootstrap/dist/css/bootstrap.min.css \
     vendor/jquery/dist/jquery.min.js \
-    vendor/jquery.lazyload/jquery.lazyload.min.js \
     vendor/underscore/underscore.js \
     vendor/backbone/backbone.js \
     ~/Dropbox/Public/movies/

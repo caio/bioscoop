699a2b464b81cf2ac39935538b3aa826649c7468
Caio Romão <caioromao@gmail.com>
2014-05-30 08:39:05 -0300

    Make Models.Movie provide its thumbnail URL

--- app/js/models/movie.js
+++ app/js/models/movie.js
@@ -13,5 +13,18 @@ MovieList.Models.Movie = Backbone.Model.extend({
         dateadded: null,
         playcount: 0,
         lastplayed: null,
-    }
+    },
+
+    thumbnailUrl: function() {
+        return "app/img/thumbs/" + this.get("id") + ".jpg";
+    },
+
+    toJSON: function () {
+        return _.extend(
+            Backbone.Model.prototype.toJSON.apply(this),
+            {
+                thumbnail: this.thumbnailUrl(),
+            }
+        );
+    },
 });

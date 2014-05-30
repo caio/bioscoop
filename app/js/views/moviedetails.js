62ae1e269cd3ef0409a27b143d632af77f99d69c
Caio Romão <caioromao@gmail.com>
2014-05-30 12:25:18 -0300

    Hack up a way to make moviedetails centered on small devices

--- app/js/views/moviedetails.js
+++ app/js/views/moviedetails.js
@@ -1,6 +1,6 @@
 MovieList.Views.MovieDetails = Backbone.View.extend({
     tagName: 'div',
-    className: 'media col-md-offset-3 col-lg-offset-3 col-sm-offset-1 col-md-6 col-lg-6 col-sm-6',
+    className: 'col-md-offset-3 col-lg-offset-3 col-sm-offset-2 col-md-6 col-lg-6 col-sm-8',
     template: _.template($('#tmpl-moviedetails').html()),
 
     render: function() {

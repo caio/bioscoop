c0c063d2e6bb25625801f759e8fae0ba7f87bdd7
Caio Romão <caioromao@gmail.com>
2014-06-01 18:50:51 -0300

    Publish bootstrap javascript file

--- publish.sh
+++ publish.sh
@@ -7,6 +7,7 @@ rsync -avRt \
     index.html \
     vendor/bootstrap/dist/css/bootstrap.min.css \
     vendor/bootstrap/dist/fonts \
+    vendor/bootstrap/dist/js/bootstrap.min.js \
     vendor/jquery/dist/jquery.min.js \
     vendor/jquery.lazyload/jquery.lazyload.min.js \
     vendor/underscore/underscore.js \
